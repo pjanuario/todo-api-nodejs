d63b14290ab64a748bf33f6f75a084a0894dcfaa
Ashley-Gautama <AshleyGautama.com>
2016-11-13 18:02:02 +0100

    added create and delete todo feature

--- routes/todo.js
+++ routes/todo.js
@@ -28,3 +28,31 @@ module.exports = {
       //   res.redirect("/");
       // }
     },
+
+    create: function(req, res, next){
+      if (!req.body || !req.body.content || !req.body.due_date || !req.body.priority)
+        return res.render("error", {error: "Please fill in all the  fields!" });
+
+
+        // create todo
+      var todo=new Todo({
+
+        name:req.user.name,
+        content:req.body.content,
+        priority:req.body.priority,
+        due_date:req.body.due_date,
+      });
+        todo.save(function (err) {if (err) console.log ('Error on save!')});
+
+        res.redirect('/todos');
+
+
+    },
+    destroy: function(req, res){
+        var id = req.params.id;
+
+        Todo.findByIdAndRemove(id, function(err, todo){
+            if(err) res.render('error', { error: 'Error deleting todo'});
+            res.redirect('/todos');
+        });
+    },
