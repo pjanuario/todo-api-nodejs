d474751649a54265a1a5b0f620c3b6a0f6a338c7
Armando Ramírez Vila <armando.ramirez.vila@gmail.com>
2017-08-20 10:32:45 +0200

    Added minimal swagger configuration

--- app/routes/task.routes.js
+++ app/routes/task.routes.js
@@ -5,8 +5,26 @@ const express = require('express');
 
 const router = express.Router();
 
+  /**
+   * @swagger
+   * /:
+   *   get:
+   *     description: Returns a task given their identifier.
+   *     responses:
+   *       200:
+   *         description: A task json object.
+   */
 router.route('/:id').get(TaskCtrl.find);
 
+  /**
+   * @swagger
+   * /:
+   *   get:
+   *     description: Returns all tasks applying filtering (offset, limit).
+   *     responses:
+   *       200:
+   *         description: A lis of task JSON objects.
+   */
 router.route('/').get(TaskCtrl.list);
 
 module.exports = router;
