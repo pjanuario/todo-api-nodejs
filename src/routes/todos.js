0d30faadb35be597120e7047d32d91333a9d199f
Farid Neshat <FaridN_SOAD@yahoo.com>
2017-11-07 14:40:28 +0800

    Add the ablity to add new items

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -10,11 +10,33 @@ const router = require('express-promise-router')();
  *       - application/json
  *     responses:
  *       200:
- *         description: An array of Todo items
+ *         description: returns an array of Todo items
  *         schema:
  *           $ref: '#/definitions/Todo'
  */
 router.get('/', (req, res) =>
   Todo.find().then(todos => res.status(200).send(todos)));
+
+/**
+ * @swagger
+ * /todos:
+ *   post:
+ *     description: create a Todo item
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: todo
+ *         description: Todo object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Todo'
+ *     responses:
+ *       201:
+ *         description: Returns the created Todo item
+ */
+router.post('/', (req, res) =>
+  Todo.create(req.body).then(todo => res.status(201).send(todo)));
+
 
 module.exports = router;
--- test/todo.test.js
+++ test/todo.test.js
@@ -21,4 +21,28 @@ describe('todo', () => {
         .then(res => expect(res.body[0].text).toEqual('sd'));
     });
   });
+
+  describe('create', () => {
+    it('simple', () => {
+      Todo.create = (obj) => {
+        const todo = new Todo(obj);
+        return todo.validate().then(() => todo);
+      };
+      const text = 'something';
+      return request(app)
+        .post('/todos')
+        .send({ text })
+        .expect(201)
+        .then(res => expect(res.body).toEqual(expect.objectContaining({
+          _id: expect.any(String),
+          text,
+        })));
+    });
+    it('invalid', () =>
+      request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .then());
+  });
 });
