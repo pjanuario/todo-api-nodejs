05a1da5049535d8322ffc60067ed3f26d2f54b63
Farid Neshat <FaridN_SOAD@yahoo.com>
2017-11-08 13:51:48 +0100

    Make logging more advanced

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const DEFAULT_PORT = 8080;
 
 require('dotenv').config();
 const express = require('express');
-const log = require('loglevel');
 const todos = require('./routes/todos');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const expressLogging = require('express-logging');
+const log = require('loglevel-message-prefix')(require('loglevel'), {
+  prefixes: ['timestamp', 'level'],
+});
 
+const jsonErrorHandler = require('express-json-error-handler');
+
+const loglevel = process.env.LOG_LEVEL || 'info';
 const dbUri = process.env.DATABASE || 'mongodb://localhost/test';
 const port = process.env.PORT || DEFAULT_PORT;
 
@@ -21,6 +27,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressLogging(log));
 app.use('/todos', todos);
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
@@ -30,10 +37,12 @@ app.use((err, req, res, next) => {
     next(err);
   }
 });
+app.use(jsonErrorHandler.default());
+
+log.setLevel(loglevel);
 
 if (module.parent === null) {
   app.listen(port, () => log.info(`Example app listening on port ${port}!`));
 }
-
 
 module.exports = app;
